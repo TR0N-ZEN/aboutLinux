8c0ab6db9230597abe2a8cd48d6b677d94a5d1b1
TR0N-ZEN <theo.reichert7@gmail.com>
2019-09-18 01:28:34 +0200

    first commit

---
+++ Ubuntu_18.04_setup.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+echo "I will setup your OS with some apps"
+deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 931FF8E79F0876134EDDBDCCA87FF9DF48BF1C90
+sudo apt-get update
+sudo apt-get upgrade
+sudo snap install --classic code # or code-insiders
+sudo apt install git
+git --version
+git config --global user.name "Theo"
+git config --global user.email "theo.reichert7@gmail.com"
+git config --list
+sudo apt-get install spotify-client
+sudo snap install discord --classic
