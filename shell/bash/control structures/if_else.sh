81778fbd36157c1c4db0f0dae59610c3b4c87c50
TR0N-ZEN <theo.reichert7@gmail.com>
2022-03-22 18:39:40 +0100

    bash syntax

---
+++ shell/bash/control structures/if_else.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [  ];then
+  
+else
+  
+fi
+
+# or
+
+if [  ]
+then
+  
+else
+  
+fi
+
+# keep a space after [ and a space before ]
+# on some occasions (I will insert one here if I don't forget) it is necessary to write [[  ]] instead of [  ], I think if you use the logical and && like [[  && ]]
+# either write if [  ] and then in one line and separate them with a scemicolon or write them on different lines
+# my source: https://stackoverflow.com/questions/50117346/why-do-some-people-put-a-semicolon-after-an-if-condition-in-shell-scripts
