9b350f3921597cea2f19f89ff3514ef073280ec6
TR0N-ZEN <theo.reichert7@gmail.com>
2022-03-24 17:12:30 +0100

    made install work

--- programs/steamcmd/DedicatedServers/GarrysMod/install.sh
+++ programs/steamcmd/DedicatedServers/GarrysMod/install.sh
@@ -5,7 +5,10 @@
 
 steamcmd +force_install_dir ~/GarrysModDS +login anonymous +app_update 4020 validate +quit
 
-sudo cp ./GarrysModDS.service /etc/systemd/system/
-sudo cp ./start.sh ~/GarrysModDS/
+touch ~/GarrysModDS/start.sh
+cp ./start.sh ~/GarrysModDS/
 sudo chmod +x ~/GarrysModDS/start.sh
-
+sudo chown steam:steam ~/GarrysModDS/start.sh
+echo 'Remember to replace <x> inside ~/GarrysModDS/start.sh with a steam-gameserver-id :->'
+sudo cp ./GarrysModDS.service /etc/systemd/system/
+sudo systemctl daemon-reload
