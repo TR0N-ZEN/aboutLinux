0a0ca98a9926abaa18fe74c3d762986910721371
TR0N-ZEN <theo.reichert7@gmail.com>
2022-03-24 15:48:40 +0100

    Update install.sh
    fixed decompression

--- programs/steamcmd/install.sh
+++ programs/steamcmd/install.sh
@@ -4,7 +4,7 @@
 
 mkdir /home/steam/steamcmd/
 cd /home/steam/steamcmd
-curl http://media.steampowered.com/installer/steamcmd_linux.tar.gz | tar -xf -
+curl http://media.steampowered.com/installer/steamcmd_linux.tar.gz | tar -xzf -
 echo "alias steamcmd='/home/steam/steamcmd/steamcmd.sh'" >> /home/steam/.bashrc
 source /home/steam/.bashrc # not sure if necessary or just open new shell
 sudo apt-get install lib32gcc1
