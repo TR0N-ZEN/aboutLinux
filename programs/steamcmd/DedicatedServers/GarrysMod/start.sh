ad38ff2d9817b043226f5eef22c6641815ccdcb9
TR0N-ZEN <theo.reichert7@gmail.com>
2022-03-24 11:40:47 +0100

    Update start.sh

--- programs/steamcmd/DedicatedServers/GarrysMod/start.sh
+++ programs/steamcmd/DedicatedServers/GarrysMod/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-. /home/steam/GarrysModDS/srcds_run -console -game garrysmod +gamemode terrortown +sv_setsteamaccount DAE93021190AB95159DAF37675B004F6 +host_workshop_collection 1526838171 +maxplayers 12 +map ttt_metropolis
+. /home/steam/GarrysModDS/srcds_run -console -game garrysmod +gamemode terrortown +sv_setsteamaccount <x> +host_workshop_collection 1526838171 +maxplayers 12 +map ttt_metropolis
