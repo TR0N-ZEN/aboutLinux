01f6be939616b1b33e5f64029b93356b1db8556e
TR0N-ZEN <theo.reichert7@gmail.com>
2022-08-05 14:10:44 +0200

    strange while loop

--- shell/bash/data/array/1.sh
+++ shell/bash/data/array/1.sh
@@ -2,15 +2,17 @@
 
 function is_in
 {
+  echo $1
   local arr=("$@")
   local i=1
   local v=0
-  while [ $i -lt $# ]; do
-    let "++i"
-    v=`echo ${arr[$i]} | sed -n '/'"$1"'/' | wc -l`
-    if [ $v -gt 0 ]; then return 1; fi
+  while [ $i -ne $# ]; do
+    let "i++"
+    echo "$i  ${arr[$i]}"
+    v=`echo ${arr[$i]} | sed -n '/'"$1"'/p' | wc -l`
+    if [ $v -gt 0 ]; then echo "LOL"; return 1; fi
   done
-  return 0
+  return 0;
 }
 
 echo `is_in "a" "b" "c" "a"`
