363db18c829361207f41b3ea9dbad23837a06f22
TR0N-ZEN <theo.reichert7@gmail.com>
2022-10-28 14:13:38 +0200

    starting with awk

---
+++ programs/awk/listing_001.sh
@@ -0,0 +1,17 @@
+awk '{print $1}' /etc/passwd
+awk -F ":" '{print $1}' /etc/passwd
+df -h | awk '{ $2 - $3}'
+df -h | awk '{ $2  $3}'
+df -h | awk '{ $2,$3}'
+df -h | awk '{print $2,$3}'
+df -h | awk '{print $2 - $3}'
+df | awk '{print $2 - $3}'
+df -h | awk '{print $2 - $3} END {print lol}' 
+df -h | awk '{print $2 - $3} END {print "lol"}' 
+df | awk '{print $2 - $3} END {print "lol"}' 
+df | awk 'BEGIN {print "lal"} {print $5} END {print "lel"}' 
+df | awk 'BEGIN {print "lal"} /dev/ {print $5} END {print "lel"}' 
+df | awk 'BEGIN {print "lal"} /dev/ {print $NF} END {print "lel"}' 
+df | awk 'BEGIN {print "lal"} /dev/ {print $1 $NF} END {print "lel"}' 
+df | awk 'BEGIN {print "lal"} /dev/ {print $1"\t"$NF} END {print "lel"}' 
+df | awk 'BEGIN {print "lal"} /dev/ {print $1"\t"$5"\t"$NF} END {print "lel"}' 
