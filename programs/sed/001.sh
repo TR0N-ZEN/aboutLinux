19e827410a4376bc82e9bca1c2732009f93652ae
TR0N-ZEN <theo.reichert7@gmail.com>
2023-10-19 22:01:25 +0200

    sed using extended regular expressions with flag -E

--- programs/sed/001.sh
+++ programs/sed/001.sh
@@ -19,9 +19,9 @@ sed -n 's/\([0-9]\{8\}\)-\([0-9]\{6\}\)/\2/p' data.txt
 
 # supresses print of everything in the <something>-buffer and prints the matched parts after the substituion
 sed \
--n \
-'s/\([0-9]\{4\}\)\([0-9]\{2\}\)\([0-9]\{2\}\)-\([0-9]\{2\}\)\([0-9]\{2\}\)\([0-9]\{2\}\)/Year:\1  Month:\2  Day:\3 Hour:\4  Minute:\5 Second:\6/p' \
-data.txt
+  -n \
+  's/\([0-9]\{4\}\)\([0-9]\{2\}\)\([0-9]\{2\}\)-\([0-9]\{2\}\)\([0-9]\{2\}\)\([0-9]\{2\}\)/Year:\1  Month:\2  Day:\3 Hour:\4  Minute:\5 Second:\6/p' \
+  data.txt
 
 # as previous example but with different substitution and piped to jq (a JSON parser)
 sed -n \
@@ -29,9 +29,14 @@ sed -n \
     data.txt | 
 jq .
 
+# as previous example but with extended regular expressions
+sed -nE \
+    's/([0-9]{4})([0-9]{2})([0-9]{2})-([0-9]{2})([0-9]{2})([0-9]{2})/{ "Year":"\1", "Month":"\2", "Day":"\3", "Hour":"\4", "Minute":"\5", "Second":"\6" }/p' \
+    data.txt | 
+jq .
+
 
 # prints all names of files ending on .md without printing .md
 ls -m1 | \
 sed -n 's/\(.*\)\.md/\1/p'
-
 
